6df9b016efbe95ec012bf37edc89b1fabd1a6012
SlendyBones <lauti122008@hotmail.com>
2021-09-23 23:09:05 -0300

    Entity

---
+++ Magic-Game/Assets/Scrips/Player/Entity.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public float life;
+    public float mana;
+
+    public void TakeDamage(float dmg)
+    {
+        life -= dmg;
+
+        if(life <= 0)
+        {
+            Death();
+        }
+
+    }
+
+    public virtual void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    public void UseMana(float um)
+    {
+        
+
+        if (mana > um)
+        {
+            mana -= um;
+        }
+      
+    }
+}
