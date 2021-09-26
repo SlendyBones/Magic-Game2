0af6741710443cb21b9e1445ae028e3a9a36f7f4
SlendyBones <lauti122008@hotmail.com>
2021-09-25 22:07:49 -0300

    Pj befor change places of scrips
    zoi todo un ingleso (?

--- Magic-Game/Assets/Scrips/Enemy/Enemy.cs
+++ Magic-Game/Assets/Scrips/Enemy/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
+    public float damage;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,13 @@ public class Enemy : Entity
     void Update()
     {
         
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Pj>())
+        {
+            collision.gameObject.GetComponent<Pj>().PlayerDamage(damage);
+        }
     }
 
 }
---
+++ Magic-Game/Assets/Scrips/UI/ScripsTempos/Pj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pj : Entity
+{
+    public HealthBar healthBar;
+    public void  PlayerDamage(float dmg)
+    {
+        
+        TakeDamage(dmg);
+        healthcheck();
+    }
+
+    public void healthcheck()
+    {
+        healthBar.SetHealth(life);
+    }
+}
