e63a91e69f5bfd4300bf0138ed5b157944dad33f
SlendyBones <lauti122008@hotmail.com>
2021-09-26 18:13:08 -0300

    eSTA COSA ENEMIGA

--- Magic-Game/Assets/Scrips/Enemy/Enemy.cs
+++ Magic-Game/Assets/Scrips/Enemy/Enemy.cs
@@ -4,24 +4,88 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
-    public float damage;
+    [SerializeField]
+    float _damage;
+    [SerializeField]
+    GameObject _bullet;
+    [SerializeField]
+    Transform _bulletSpawnPoint;
+    
+    [SerializeField]
+    float _attackDistance = 10f;
+    [SerializeField]
+    float _followDistance = 20f;
+
+    bool shoot = false;
+    bool follow = false;
+    [SerializeField]
+    float _speed;
+    private PatrolEnemy _patrolEnemy;
+
+    public Transform player;
+
+
     // Start is called before the first frame update
     void Start()
     {
         
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        _patrolEnemy = gameObject.GetComponent<PatrolEnemy>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float dist = Vector3.Distance(player.transform.position, this.transform.position);
+        Debug.Log(dist);
+        if (dist < _followDistance)
+        {
+            follow = true;
+            if (follow==true)
+            {
+                _patrolEnemy.start = false;
+                if (dist < _attackDistance)
+                {
+                    shoot = true;
+                    Shoot();
+                    Debug.Log("pui");
+                }
+                else
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, player.position, _speed * Time.deltaTime);
+                    Debug.Log("Jose");
+                }
+            }
+
+            else if (shoot)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, transform.position, _speed * Time.deltaTime);
+                Debug.Log("Josa");
+
+            }
+          
+
+        }
+        else if (follow == false)
+        {
+            _patrolEnemy.start = true;
+            Debug.Log("kill me");
+        }
+
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<Pj>())
         {
-            collision.gameObject.GetComponent<Pj>().PlayerDamage(damage);
+            collision.gameObject.GetComponent<Pj>().PlayerDamage(_damage);
         }
+    }
+
+    void Shoot()
+    {
+        Instantiate(_bulletSpawnPoint, transform.position, transform.rotation);
     }
 
 }
--- Magic-Game/Assets/Scrips/Enemy/PatrolEnemy.cs
+++ Magic-Game/Assets/Scrips/Enemy/PatrolEnemy.cs
@@ -9,26 +9,30 @@ public class PatrolEnemy : MonoBehaviour
     public float speed;
 
     public Transform player;
-   
 
+    public bool start;
     // Start is called before the first frame update
     void Start()
     {
         current = 0;
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position != points[current].position)
+        if (start == true)
         {
-            transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
+            if (transform.position != points[current].position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
+            }
+            else
+            {
+                current = (current + 1) % points.Length;
+            }
         }
-        else
-        {
-            current = (current + 1) % points.Length;
-        }
+       
     }
 
     
