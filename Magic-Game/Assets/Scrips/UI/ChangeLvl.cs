aa75300d81d7d492659a60ed0275e0cc91296b97
SlendyBones <lauti122008@hotmail.com>
2021-09-30 17:29:26 -0300

    Instrucciones
    Como me caigo a pedazos :'v

--- Magic-Game/Assets/Scrips/UI/ChangeLvl.cs
+++ Magic-Game/Assets/Scrips/UI/ChangeLvl.cs
@@ -7,8 +7,20 @@ public class ChangeLvl : MonoBehaviour
 {
     [SerializeField]
     string _lvl;
+    [SerializeField]
+    string _anotherScene;
     public void ChangeLVL()
     {
         SceneManager.LoadScene(_lvl);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void AnotherScene()
+    {
+        SceneManager.LoadScene(_anotherScene);
     }
 }
