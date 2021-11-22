c432aa5505c7d38654605b6f3cdb05866c76609a
SlendyBones <lauti122008@hotmail.com>
2021-11-22 19:31:43 -0300

    Jose

--- Magic-Game/Assets/Scrips/Enemy/SpawnEnemy.cs
+++ Magic-Game/Assets/Scrips/Enemy/SpawnEnemy.cs
@@ -4,21 +4,5 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    [SerializeField]
-    private List<GameObject> Enemys;
-    private float timer;
-
-    //Esto deberia ir en el GameManager
-    private int amountOfEnemy;
-    public int AmountOfEnemy { set { amountOfEnemy--; } }
-
-    void Update()
-    {
-        if(amountOfEnemy < 15 && timer > 1)
-        {
-            int whitchEnemy = Random.Range(0, 3);
-            Instantiate(Enemys[whitchEnemy], transform.position, transform.rotation);
-        }
-        timer += 1 * Time.deltaTime;
-    }
+  
 }
---
+++ Magic-Game/Assets/Scrips/Enemy/SpawnTrapFireBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrapFireBall : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _fireBall;
+    [SerializeField]
+    private float _timer;
+    [SerializeField]
+    private float _timeforspawn;
+
+
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (_timer <= 0)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                int _spawnFireBall = (Random.Range(0, 6));
+                Instantiate(_fireBall[_spawnFireBall], transform.position, transform.rotation);
+            }
+            _timer = _timeforspawn;
+        }
+    }
+
+}
