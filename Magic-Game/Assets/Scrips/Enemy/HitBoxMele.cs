aebb42ae400832da6a59ca431dd47da41981f7da
MaiineV <agustinmartinezbarrios2@hotmail.com>
2021-12-08 20:27:15 -0300

    Aca ta
    MeleEnemy y EnemyHitBox creo que se llaman los dos nuevos

---
+++ Magic-Game/Assets/Scrips/Enemy/HitBoxMele.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBoxMele : MonoBehaviour
+{
+    //La hitbox que se apaga y se prende al golpear
+    [SerializeField] private float _dmg;
+    [SerializeField] private float _force;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EventManager.Trigger("PlayerDamage", _dmg);
+        Transform playerTransform = other.gameObject.GetComponent<Transform>();
+        playerTransform.transform.position += ((transform.position - playerTransform.transform.position) + transform.up) * _force * Time.deltaTime;
+        gameObject.SetActive(false);
+    }
+}
---
+++ Magic-Game/Assets/Scrips/Enemy/MeleEnemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleEnemy : Entity
+{
+   //Enemigo, movimiento, falta poner animacion namas y valores en inspector
+    private bool isAtack = false;
+
+    private delegate void Moving();
+    Moving _move;
+
+    [SerializeField] private GameObject _atackHitBox;
+
+    private void Start()
+    {
+        _move = Movement;
+    }
+    void Update()
+    {
+        _move();
+
+        distancePlayer = Vector3.Distance(player.transform.position, transform.position);
+
+        if(distancePlayer <= attackDistance && isAtack == false)
+        {
+            isAtack = true;
+            //animacion de ataque
+        }
+    }
+
+    private void Movement()
+    {
+        transform.forward = transform.position - player.position;
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    //Se llama al inicio de la animacion
+    public void StopMoving()
+    {
+        _move = delegate { };
+    }
+
+    //Se llama cuando quieras iniciar el collider del ataque
+    public void AttackOn()
+    {
+        _atackHitBox.gameObject.SetActive(true);
+    }
+
+    //Llamar al final de la animacion para poder volver a caminar
+    public void ContinousMoving()
+    {
+        isAtack = false;
+        _atackHitBox.gameObject.SetActive(false);
+        _move = Movement;
+    }
+}
