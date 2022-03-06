48e302e795deda3509eefe9efde9f98998633db7
MaiineV <agustinmartinezbarrios2@hotmail.com>
2022-03-06 18:55:48 -0300

    COSAS

---
+++ Magic-Game/Assets/Scrips/Abilities/AtractedScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtractedScript : MonoBehaviour
+{
+    private bool _isAtracting;
+    private Vector3 _dirVect;
+    
+    void Update()
+    {
+        if (_isAtracting)
+        {
+
+        }
+    }
+
+    public void AtractState(bool state)
+    {
+        _isAtracting = state;
+    }
+}
--- Magic-Game/Assets/Scrips/Abilities/BlackHoleFuncion.cs
+++ Magic-Game/Assets/Scrips/Abilities/BlackHoleFuncion.cs
@@ -7,19 +7,19 @@ public class BlackHoleFuncion : MonoBehaviour
 {
     [SerializeField] 
     float _gravityPull = .78f;
-    public static float m_GravityRadius = 1f;
-    [SerializeField] private LayerMask enemy;
-    void Awake()
+    public float m_GravityRadius = 1f;
+    [SerializeField] private LayerMask AtractedObjects;
+    /*void Awake()
     {
         m_GravityRadius = GetComponent<SphereCollider>().radius;
     }
-    
+
     void OnTriggerStay(Collider other)
     {
         if (other.attachedRigidbody)
         {
            
-            if(other.gameObject.layer == enemy)
+            if(other.gameObject.layer == AtractedObjects)
             {
                 float gravityIntensity = Vector3.Distance(transform.position, other.transform.position) / m_GravityRadius;
                 other.attachedRigidbody.AddForce((transform.position - other.transform.position) * gravityIntensity * other.attachedRigidbody.mass * _gravityPull * Time.deltaTime);
@@ -34,7 +34,7 @@ public class BlackHoleFuncion : MonoBehaviour
         if (other.attachedRigidbody)
         {
 
-            if (other.gameObject.layer == enemy)
+            if (other.gameObject.layer == AtractedObjects)
             {
                 float gravityIntensity = Vector3.Distance(transform.position, other.transform.position) / m_GravityRadius;
                 other.attachedRigidbody.AddForce((transform.position - other.transform.position) * gravityIntensity * other.attachedRigidbody.mass * _gravityPull * Time.deltaTime);
@@ -48,12 +48,30 @@ public class BlackHoleFuncion : MonoBehaviour
         if (other.attachedRigidbody)
         {
 
-            if (other.gameObject.layer == enemy)
+            if (other.gameObject.layer == AtractedObjects)
             {
                 float gravityIntensity = Vector3.Distance(transform.position, other.transform.position) / m_GravityRadius;
                 other.attachedRigidbody.AddForce((transform.position - other.transform.position) * gravityIntensity * other.attachedRigidbody.mass * _gravityPull * Time.deltaTime);
                 Debug.DrawRay(other.transform.position, transform.position - other.transform.position);
             }
+        }
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        AtractedScript thisAtracted = other.gameObject.GetComponent<AtractedScript>();
+        if ( thisAtracted != null)
+        {
+            thisAtracted.AtractState(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        AtractedScript thisAtracted = other.gameObject.GetComponent<AtractedScript>();
+        if ( thisAtracted != null)
+        {
+            thisAtracted.AtractState(false);
         }
     }
 }
